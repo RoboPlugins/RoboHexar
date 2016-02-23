e58337d21edc850ebbaac2e20e5085c25b5f04e0
Robert Wallis <smilingrob@gmail.com>
2016-02-23 10:48:13 -0800

    Test file for testing successful cases.

---
+++ testData/JavaDocGoodWithLinks.java
@@ -0,0 +1,16 @@
+/**
+ * This is a test with links {@link #JavaDocGood} in
+ * the middle of a sentence.
+ */
+public class JavaDocGood {
+
+    /**
+     * This is a function with links {@link #JavaDocGood} in
+     * the middle of a sentence.
+     *
+     * @param param This parameter also has good grammar in the Java docs.
+     */
+    public void func(int param) {
+        param += 1;
+    }
+}
