1481d4d2c6608115791f59aa8ac1a171cda01073
Phil Leder and Will Kamp <phil.leder+wkamp@nordstrom.com>
2016-03-01 22:57:51 -0800

    Prepare script still needs work.

--- prepare-archive.sh
+++ prepare-archive.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+git config --global user.name $GIT_AUTHOR_NAME
+git config --global user.email $GIT_AUTHOR_EMAIL
 
 cd /tmp
 git clone https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} clonedir
-mkdir ${GH_PROJECT_NAME}
-cd ${GH_PROJECT_NAME}/
-mkdir libs
+cd clonedir
+git pull origin/master
 cd ..
-ls $TRAVIS_BUILD_DIR/build/libs/
-cp $TRAVIS_BUILD_DIR/build/libs/${GH_PROJECT_NAME}-*.jar ${GH_PROJECT_NAME}/libs/
+mkdir ${GH_PROJECT_NAME}
+mkdir ${GH_PROJECT_NAME}/libs
+cp $TRAVIS_BUILD_DIR/build/libs/*.jar ${GH_PROJECT_NAME}/libs/
 zip ${GH_PROJECT_NAME}.zip ${GH_PROJECT_NAME}/
 cp ${GH_PROJECT_NAME}.zip clonedir/
 cd clonedir
-git config --global user.name $GIT_AUTHOR_NAME
-git config --global user.email $GIT_AUTHOR_EMAIL
 git add ${GH_PROJECT_NAME}.zip
 git commit -m "Committed by Travis-CI"
 git push https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_REPO_NAME} 2>&1
