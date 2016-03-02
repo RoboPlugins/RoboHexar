2876e597d4f4fc4e3239d7e56f03b3e0135f519e
Phil Leder <phil.leder@nordstrom.com>
2016-03-02 12:27:06 -0800

    Updating more logging for the prepare script

--- prepare-archive.sh
+++ prepare-archive.sh
@@ -12,44 +12,44 @@ git reset
 git pull https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_REPO_NAME}
 
 # Create Directories for zip
-echo "\n2. Create Directories for zip"
+echo "\n 2. Create Directories for zip"
 cd /tmp
 mkdir ${GH_PROJECT_NAME}
 mkdir ${GH_PROJECT_NAME}/libs
 
 # Take a look.
-echo "\n3. Take a look."
+echo "\n 3. Take a look."
 ls $TRAVIS_BUILD_DIR/build/libs
 ls $TRAVIS_BUILD_DIR/libs/*.jar
 
 # copy jars to directory
-echo "\n4. copy jars to directory."
+echo "\n 4. copy jars to directory."
 
 cp -R $TRAVIS_BUILD_DIR/build/libs* ${GH_PROJECT_NAME}/libs/
 cp -R $TRAVIS_BUILD_DIR/libs/*.jar ${GH_PROJECT_NAME}/libs/
 
 # Take a look in our zip directory
-echo "\n5. Take a look in our zip directory:"
+echo "\n 5. Take a look in our zip directory:"
 ls ${GH_PROJECT_NAME}/libs/
 
 # Zip it
-echo "\n6. Zip it:"
+echo "\n 6. Zip it:"
 zip ${GH_PROJECT_NAME}.zip ${GH_PROJECT_NAME}/*
 
 # Go to clone we created earlier.
-echo "\n7. Go to clone we created earlier.:"
+echo "\n 7. Go to clone we created earlier.:"
 cd /tmp/clonedir
 
 # Copy the new zip to the clone of the repo
-echo "\n8. Copy the new zip to the clone of the repo:"
+echo "\n 8. Copy the new zip to the clone of the repo:"
 cp ${GH_PROJECT_NAME}.zip .
 
 # Take a look.
-echo "\n9. Take a look."
+echo "\n 9. Take a look."
 ls
 
 # Add, commit, and push
-echo "\n10. Add, commit, and push:"
+echo "\n 10. Add, commit, and push:"
 git add ${GH_PROJECT_NAME}.zip
 git commit -a -m "Committed by Travis-CI"
 git push https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_REPO_NAME} 2>&1
