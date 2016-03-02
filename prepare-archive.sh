8d78022ca938d953ee1c3b529a85be9d4f2b85e1
Phil Leder and Will Kamp <phil.leder+wkamp@nordstrom.com>
2016-03-01 22:42:32 -0800

    update prepare script.

--- prepare-archive.sh
+++ prepare-archive.sh
@@ -2,11 +2,15 @@
 
 cd /tmp
 git clone https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} clonedir
-cd clonedir
+mkdir ${GH_PROJECT_NAME}
+cd ${GH_PROJECT_NAME}/
 mkdir libs
+cd ..
 ls $TRAVIS_BUILD_DIR/build/libs/
-cp $TRAVIS_BUILD_DIR/build/libs/${GH_PROJECT_NAME}-*.jar libs/
-zip ${GH_PROJECT_NAME}.zip libs/
+cp $TRAVIS_BUILD_DIR/build/libs/${GH_PROJECT_NAME}-*.jar ${GH_PROJECT_NAME}/libs/
+zip ${GH_PROJECT_NAME}.zip ${GH_PROJECT_NAME}/
+cp ${GH_PROJECT_NAME}.zip clonedir/
+cd clonedir
 git config --global user.name $GIT_AUTHOR_NAME
 git config --global user.email $GIT_AUTHOR_EMAIL
 git add ${GH_PROJECT_NAME}.zip
