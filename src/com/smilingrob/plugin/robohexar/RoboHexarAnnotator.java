b993b47d71534c8eabb18ba527febd5538c7b50d
Schlank <phil@philipleder.com>
2016-03-27 21:53:30 -0700

    Success with adding a hint to robohexar decorations.

--- src/com/smilingrob/plugin/robohexar/RoboHexarAnnotator.java
+++ src/com/smilingrob/plugin/robohexar/RoboHexarAnnotator.java
@@ -5,6 +5,7 @@ import com.intellij.codeInsight.hint.HintManagerImpl;
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
 import com.intellij.codeInsight.preview.ElementPreviewHintProvider;
 import com.intellij.ide.plugins.PluginManager;
+import com.intellij.lang.ASTNode;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.lang.annotation.HighlightSeverity;
@@ -14,6 +15,7 @@ import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.editor.event.EditorMouseEvent;
 import com.intellij.openapi.editor.event.EditorMouseEventArea;
 import com.intellij.openapi.editor.event.EditorMouseListener;
+import com.intellij.openapi.editor.ex.util.EditorUtil;
 import com.intellij.openapi.editor.ex.util.LayerDescriptor;
 import com.intellij.openapi.editor.markup.EffectType;
 import com.intellij.openapi.editor.markup.HighlighterLayer;
@@ -23,10 +25,8 @@ import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Key;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementFactory;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.*;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.xml.XmlElement;
 import com.intellij.psi.xml.XmlTag;
@@ -40,6 +40,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import javax.swing.text.Position;
 import java.awt.*;
 import java.lang.reflect.Type;
 
@@ -56,12 +57,10 @@ public class RoboHexarAnnotator implements Annotator {
         GrammarParser grammarParser = new GrammarParser();
         for (GrammarError error : grammarParser.parse(psiElement)) {
             annotationHolder.createWeakWarningAnnotation(error.rangeOfError(), error.messageForError());
-//            annotateElement(error.getElementToTag(), error.messageForError());
         }
 
         JavaDocParser javaDocParser = new JavaDocParser();
         for (JavaDocError error : javaDocParser.parse(psiElement)) {
-//            annotationHolder.createWeakWarningAnnotation(error.getElementToTag(), error.messageForError());
             annotateElement(error.getElementToTag(), error.messageForError());
         }
 
@@ -77,19 +76,6 @@ public class RoboHexarAnnotator implements Annotator {
 
         final TextAttributes textattributes = new TextAttributes(null, null, JBColor.RED, EffectType.WAVE_UNDERSCORE, Font.ITALIC);
 
-//        MyPsiElementCellRenderer renderer = new MyPsiElementCellRenderer(DomManager.getDomManager(element.getProject()));
-//        if (elements.size() > 1) {
-//            icon = AllIcons.Icons.W16;
-//        } else {
-//            icon = renderer.getIcon(Iterables.getFirst(elements, null));
-//        }
-
-
-//        builder.setNamer(new XmlFileByElementNamer());
-//        builder.setCellRenderer(renderer);
-//        result.add(builder.createLineMarkerInfo(identifier));
-
-
         ApplicationManager.getApplication().invokeLater(new Runnable() {
             public void run() {
                 final Project project = psiElement.getProject();
@@ -102,72 +88,47 @@ public class RoboHexarAnnotator implements Annotator {
                             HighlighterLayer.WARNING,
                             textattributes,
                             HighlighterTargetArea.EXACT_RANGE);
+
+                    editor.addEditorMouseListener(new EditorMouseListener() {
+                        @Override
+                        public void mousePressed(EditorMouseEvent editorMouseEvent) {
+                        }
+
+                        @Override
+                        public void mouseClicked(EditorMouseEvent editorMouseEvent) {
+                            showHint(editorMouseEvent);
+                        }
+
+                        @Override
+                        public void mouseReleased(EditorMouseEvent editorMouseEvent) {
+                        }
+
+                        @Override
+                        public void mouseEntered(EditorMouseEvent editorMouseEvent) {
+                            showHint(editorMouseEvent);
+                        }
+
+                        @Override
+                        public void mouseExited(EditorMouseEvent editorMouseEvent) {}
+
+                        private void showHint(EditorMouseEvent editorMouseEvent) {
+                            if (editorMouseEvent.getArea().equals(EditorMouseEventArea.EDITING_AREA)) {
+                                PsiIdentifier psiIdentifier = (PsiIdentifier) psiElement;
+                                TextRange textRange = EditorUtil.getSelectionInAnyMode(editor);
+                                TextRange elementTextRange = psiIdentifier.getTextRange();
+                                int mouseOffset = textRange.getStartOffset();
+                                int textStartOffset = elementTextRange.getStartOffset();
+                                int textEndOffset = elementTextRange.getEndOffset();
+                                if (mouseOffset >= textStartOffset && mouseOffset <= textEndOffset) {
+                                    HintManager.getInstance().showErrorHint(editor, message);
+                                }
+                            }
+                        }
+
+
+                    });
                 }
-
-                editor.addEditorMouseListener(new EditorMouseListener() {
-                    @Override
-                    public void mousePressed(EditorMouseEvent editorMouseEvent) {
-                        PluginManager.getLogger().warn("mousePressed");
-                    }
-
-                    @Override
-                    public void mouseClicked(EditorMouseEvent editorMouseEvent) {
-                        PluginManager.getLogger().warn("mouseClicked");
-
-                    }
-
-                    @Override
-                    public void mouseReleased(EditorMouseEvent editorMouseEvent) {
-                        PluginManager.getLogger().warn("mouseReleased");
-
-                    }
-
-                    @Override
-                    public void mouseEntered(EditorMouseEvent editorMouseEvent) {
-                        PluginManager.getLogger().warn("mouseEntered");
-
-                        Point mousePoint = editorMouseEvent.getMouseEvent().getPoint();
-//                        Point mousePoint = editorMouseEvent.getMouseEvent().getLocationOnScreen();
-
-//                        ElementPreviewHintProvider elementPreviewHintProvider = new ElementPreviewHintProvider();
-//                        elementPreviewHintProvider.show(psiElement, editor, mousePoint, false);
-
-
-                        JLabel jLabel = new JLabel(message);
-d
-                        LightweightHint lightweightHint = new LightweightHint(jLabel);
-
-                        HintManagerImpl hintManager = (HintManagerImpl)HintManager.getInstance();
-                        hintManager.showEditorHint(
-                                lightweightHint,
-                                editor,
-                                mousePoint,
-                                HintManager.HIDE_BY_ANY_KEY | HintManager.HIDE_BY_TEXT_CHANGE | HintManager.HIDE_BY_OTHER_HINT | HintManager.HIDE_BY_SCROLLING,
-                                0,
-                                false);
-                                HintManagerImpl.createHintHint(editor,mousePoint,lightweightHint,HintManager.UNDER).setAwtTooltip(false));
-
-                        HintManager.getInstance().showInformationHint(editor, message);
-                        HintManager.getInstance().showErrorHint(editor, message, (short)34);
-
-//                        int offset = editor.getCaretModel().getOffset();
-//                        boolean chosen = GotoDeclarationAction.chooseAmbiguousTarget(editor, offset, processor,
-//                                FindBundle.message("find.usages.ambiguous.title", "crap"), null);
-                    }
-
-                    @Override
-                    public void mouseExited(EditorMouseEvent editorMouseEvent) {
-                        PluginManager.getLogger().warn("mouseExited");
-
-                    }
-                });
-                PluginManager.getLogger().warn("Text Highlighted.");
             }
         });
-
-
-//            Annotation annotation = new Annotation(0,0, HighlightSeverity.WEAK_WARNING, error.messageForError(), error.messageForError());
-//            TextAttributesKey textAttributesKey = TextAttributesKey.createTextAttributesKey("externalName", textattributes);
-//            annotation.setTextAttributes(textAttributesKey);
     }
 }
